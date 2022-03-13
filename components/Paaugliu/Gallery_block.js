929b93e92489ea61003603b896b3ded7cce9d40d
sheten <dsablauskas@gmail.com>
2022-03-13 16:44:04 +0200

    removed 3 photos

--- components/Paaugliu/Gallery_block.js
+++ components/Paaugliu/Gallery_block.js
@@ -7,10 +7,8 @@ const Gallery_block = () => {
 
   return (
     <Wrap>
-      <Image src="/images/paaugliams1.jpg" alt="Nuotrauka" />
       <Image src="/images/paaugliams6.jpg" alt="Nuotrauka" />
       <Image src="/images/paaugliams3.jpg" alt="Nuotrauka" />
-      <Image src="/images/paaugliams4.jpg" alt="Nuotrauka" />
       <Image src="/images/paaugliams5.jpg" alt="Nuotrauka" />
       <Image src="/images/paaugliams2.jpg" alt="Nuotrauka" />
       <Image src="/images/paaugliams7.jpg" alt="Nuotrauka" />
@@ -25,7 +23,6 @@ const Gallery_block = () => {
       <Image src="/images/paaug8.jpg" alt="Nuotrauka" />
       <Image src="/images/paaug9.jpg" alt="Nuotrauka" />
       <Image src="/images/paaug10.jpg" alt="Nuotrauka" />
-      <Image src="/images/paaug11.jpg" alt="Nuotrauka" />
     </Wrap>
   )
 }
